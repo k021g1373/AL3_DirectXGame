a886d5a4967c869fe7d01e51335db0f01c35a4c8
k021g1373 <k021g1373@g.neec.ac.jp>
2022-04-25 16:45:43 +0900

    01_01_確認課題

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
 	win->CreateGameWindow("GE2A_01_アグイ_ヒデユキ_AL3");
-
+	
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
